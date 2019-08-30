17281e322030a0c7ba951e1714b732a9fc3952c6
Fernando Borges Paul <fborpol@me.com>
2019-08-30 15:59:16 -0500

    App configured to track one single image

--- MagicPaper/ViewController.swift
+++ MagicPaper/ViewController.swift
@@ -23,18 +23,21 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Show statistics such as fps and timing information
         sceneView.showsStatistics = true
         
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
-        
-        // Set the scene to the view
-        sceneView.scene = scene
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
-        // Create a session configuration
-        let configuration = ARWorldTrackingConfiguration()
+        // Create a session configuration to track images.
+        let configuration = ARImageTrackingConfiguration()
+        
+        if  let trackedImages = ARReferenceImage.referenceImages(inGroupNamed: "NewPaperImages", bundle: Bundle.main) {
+            
+            configuration.trackingImages = trackedImages  // This will track the images from our Folder NewPaperImages
+            
+            configuration.maximumNumberOfTrackedImages = 1 // Configures the numbers of pictures it will track, in this case,1.
+            
+        }
 
         // Run the view's session
         sceneView.session.run(configuration)
